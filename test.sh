93bad39c40b939e00ea2af49bfa8e4d3d076cc8d
james-otten <jamesotten1@gmail.com>
2025-02-13 01:28:14 -0500

    sn10

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+cd ansible/
+ansible-playbook tests.yaml --extra-vars "env_file=dev.yaml expected_range=10.70.248.0/24 expected_default=10.70.248.1"
+ansible-playbook tests.yaml --extra-vars "env_file=wireguard_sn10.yaml expected_range=10.70.247.0/24 expected_default=10.70.247.1"
+ansible-playbook tests.yaml --extra-vars "env_file=wireguard_sn3.yaml expected_range=10.70.250.0/24 expected_default=10.70.250.1"
